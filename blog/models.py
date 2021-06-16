4a0021eba36af9282a5302becf8c2c07cd8cc544
The0therOne <rastagrant21@gmail.com>
2021-06-17 00:51:56 +0300

    Fixed count of comments

--- blog/models.py
+++ blog/models.py
@@ -15,15 +15,15 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
-    def __str__(self):
-        return self.title
-
     def approved_comments(self):
         return self.comments.filter(approved_comment=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
     text = models.TextField(max_length=200)
     created_date = models.DateTimeField(default=timezone.now)
